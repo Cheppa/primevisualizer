578ea278d9e7cefea25533ec1e1c0e46b565ba31
Cheppa <mail.cheppa@gmail.com>
2021-11-09 21:34:13 +0200

    miklyfix

--- public/primevisualizer2/sketch.js
+++ public/primevisualizer2/sketch.js
@@ -28,6 +28,9 @@ const isPrime = (number) => {
   return true;
 };
 
+let repeat = 1;
+let fpsValue = 30;
+
 // The statements in the setup() function
 // execute once when the program begins
 function setup() {
@@ -37,7 +40,22 @@ function setup() {
   textFont("Arial");
 
   stroke(128); // Set line drawing color to white
-  frameRate(60);
+
+  const timesE = document.getElementById("times");
+  const fpsE = document.getElementById("fps");
+
+  fpsE.addEventListener("input", (e) => {
+    fpsValue = Number(e.target.value);
+    frameRate(fpsValue);
+  });
+  timesE.addEventListener("input", (e) => {
+    repeat = Number(e.target.value);
+  });
+
+  fps = Number(fpsE.value);
+  repeat = Number(timesE.value);
+
+  frameRate(fpsValue);
 }
 // The statements in draw() are executed until the
 // program is stopped. Each statement is executed in
@@ -51,44 +69,57 @@ function draw() {
   textAlign(LEFT, TOP);
   text(maxPrime(), 3, 3);
 
-  let newPrime = primes[primes.length - 1] + 1;
-  while (!isPrime(newPrime)) {
-    newPrime++;
+  for (let i = 0; i < repeat; i++) {
+    let newPrime = primes[primes.length - 1] + 1;
+    while (!isPrime(newPrime)) {
+      newPrime++;
+    }
+    if (primes.length >= width / lineWidth) {
+      diff.shift();
+      primes.shift();
+      floaters.shift();
+    }
+    const newDiff = newPrime - primes[primes.length - 1];
+    if (!counts?.[newDiff]) {
+      counts[newDiff] = 1;
+    } else {
+      counts[newDiff]++;
+    }
+    diff.push(newDiff);
+    let floater = 0;
+    if (newDiff > biggestDiff) {
+      biggestDiff = newDiff;
+      floater = newDiff;
+    }
+    floaters.push(floater);
+    primes.push(newPrime);
   }
-  if (primes.length >= width / lineWidth) {
-    diff.shift();
-    primes.shift();
-    floaters.shift();
-  }
-  const newDiff = newPrime - primes[primes.length - 1];
-  if (!counts?.[newDiff]) {
-    counts[newDiff] = 1;
-  } else {
-    counts[newDiff]++;
-  }
-  diff.push(newDiff);
-  let floater = 0;
-  if (newDiff > biggestDiff) {
-    biggestDiff = newDiff;
-    floater = newDiff;
-  }
-  floaters.push(floater);
-  primes.push(newPrime);
 
-  const countsStrokeWeight = Math.min(10, width / counts.length);
+  const countsStrokeWeight = Math.min(20, (width - 20) / counts.length);
   let maxCount = 1;
-  counts.forEach((count) => {
+  let maxCountIndex = 0;
+  counts.forEach((count, index) => {
     if (count > maxCount) {
       maxCount = count;
+      maxCountIndex = index;
     }
   });
 
   counts.forEach((count, i) => {
     strokeWeight(1);
     textSize(10);
-    textAlign(LEFT, BOTTOM);
-    text(String(i), 5 + i * countsStrokeWeight, 54);
+    textAlign(CENTER, BOTTOM);
+    text(String(i), 10 + i * countsStrokeWeight, 50);
 
+    if (maxCountIndex === i) {
+      textAlign(CENTER, BOTTOM);
+      textSize(14);
+      text(
+        String(maxCount),
+        10 + i * countsStrokeWeight,
+        85 + (count / maxCount) * 400
+      );
+    }
     strokeWeight(countsStrokeWeight);
     line(
       10 + i * countsStrokeWeight,
@@ -100,7 +131,17 @@ function draw() {
 
   diff.forEach((value, i) => {
     strokeWeight(strokeWidth);
-    line(i * lineWidth, height, i * lineWidth, height - value);
+
+    if (biggestDiff > 100) {
+      line(
+        i * lineWidth,
+        height,
+        i * lineWidth,
+        height - (value / biggestDiff) * 100
+      );
+    } else {
+      line(i * lineWidth, height, i * lineWidth, height - value);
+    }
 
     strokeWeight(1);
     if (floaters[i] > 0) {
@@ -112,8 +153,17 @@ function draw() {
 
   textSize(18);
   textAlign(RIGHT, BOTTOM);
-  text(String(biggestDiff), width - 3, height - biggestDiff - 3);
 
   strokeWeight(1);
-  line(0, height - biggestDiff, width, height - biggestDiff);
+  if (biggestDiff > 100) {
+    stroke(128);
+    line(0, height - 100, width, height - 100);
+    text(String(biggestDiff), width - 3, height - 100);
+  } else {
+    stroke(64);
+    line(0, height - 100, width, height - 100);
+    stroke(128);
+    line(0, height - biggestDiff, width, height - biggestDiff);
+    text(String(biggestDiff), width - 3, height - biggestDiff - 3);
+  }
 }
