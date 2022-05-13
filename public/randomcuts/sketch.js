ea58baf68952a8c24b80305a1c187f6ea5cf3956
Cheppa <mail.cheppa@gmail.com>
2022-05-14 02:12:12 +0300

    stuf

--- public/randomcuts/sketch.js
+++ public/randomcuts/sketch.js
@@ -1,19 +1,37 @@
-const lineWidth = 5;
+const lineWidth = 1;
+const diameter = 300;
 
 // The statements in the setup() function
 // execute once when the program begins
 function setup() {
-  createCanvas(720, 600); // Size must be the first statement
+    createCanvas(720, 600); // Size must be the first statement
 
-  stroke(255, 10); // Set line drawing color to white
-  frameRate(30);
-  background(0); // Set the background to black
+    frameRate(30);
+    background(0); // Set the background to black
+    fill(0);
 }
 
 function draw() {
+    stroke(255, 30); // Set line drawing color to white
+    //   text(maxPrime(), 3, 3);
 
-//   fill(255);
-//   text(maxPrime(), 3, 3);
-    for (let i = 0; i < 100; i++)
-    line(Math.random() * width, Math.random() * height,Math.random() * width,Math.random() * height);
+    for (let i = 0; i < 100; i++) {
+        const radius = diameter / 2;
+        const angle = Math.random() * 360;
+        const x = Math.sin(angle) * Math.random() * radius;
+        const y = Math.cos(angle) * Math.random() * radius;
+        point(width / 2 + x, height / 2 + y);
+        // point(x, y);
+        // line(
+        //     Math.random() * width,
+        //     Math.random() * height,
+        //     Math.random() * width,
+        //     Math.random() * height
+        // );
+    }
+
+    noFill();
+    stroke(color(255, 0, 0), 50); // Set line drawing color to white
+
+    circle(width / 2, height / 2, diameter);
 }
